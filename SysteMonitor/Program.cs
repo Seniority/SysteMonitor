68fcbe3799cf0efb233c0397b4130012781c8dcb
Seniority <senioritylabs@gmail.com>
2016-04-28 00:57:28 -0700

    made code more efficient, added comments, and changed Txt2Speech to female voice

--- SysteMonitor/Program.cs
+++ SysteMonitor/Program.cs
@@ -11,21 +11,26 @@ namespace SysteMonitor
 {
     class Program
     {
-        private static bool keepGoing = true;
-        private static int memoryWarning = 0;
-        private static int cpuWarning = 0;
+        public static bool keepGoing = true;
+        public static int memoryWarning = 0;
+        public static int cpuWarning = 0;
+
+        private static SpeechSynthesizer speechSynth = new SpeechSynthesizer();
 
         static void Main(string[] args)
-        {            
-            SpeechSynthesizer speechSynth = new SpeechSynthesizer();
+        {
+            // create text-to-speech object
+            speechSynth.SelectVoiceByHints(VoiceGender.Female);
             speechSynth.Speak("Welcome to System Monitor.");
 
+            // create PerformanceCounter objects that grab system info from Performance Monitor
             #region Performance Counters            
             PerformanceCounter perfCPUCount = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
             PerformanceCounter perfMemCount = new PerformanceCounter("Memory", "Available MBytes");
             PerformanceCounter perfUptimeCount = new PerformanceCounter("System", "System Up Time");
             #endregion
 
+            // main loop that prints system performance info and warns when necessary
             while (keepGoing)
             {
                 float currentCPUPercentage = (int)perfCPUCount.NextValue();
@@ -38,34 +43,31 @@ namespace SysteMonitor
                 Console.WriteLine();
                 Console.WriteLine();
 
+                // warning if CPU load reaches assigned warning threshold (set in the "if" loop below)
                 if (currentCPUPercentage > 90)
                 {
                     if (cpuWarning.ToString().EndsWith("0") || cpuWarning.ToString().EndsWith("5"))
                     {
-                        string cpuLoadVocalMessage = String.Format("The current CPU load is {0} percent", currentCPUPercentage);
-                        speechSynth.Speak(cpuLoadVocalMessage);
+                        speechSynth.Speak(String.Format("The current CPU load is {0} percent", currentCPUPercentage));
                     }
 
                     cpuWarning++;
                 }
 
+                // warning if memory drops below assigned threshold (set in the "if" loop below)
                 if (currentAvailableMemory < 800)
                 {
                     if (memoryWarning.ToString().EndsWith("0"))
                     {
-                        string memoryAvailableVocalMessage = String.Format("Warning. Your available memory is {0} megabytes.", currentAvailableMemory);
-                        speechSynth.Speak(memoryAvailableVocalMessage);
+                        speechSynth.Speak(String.Format("Warning. Your available memory is {0} megabytes.", currentAvailableMemory));
                     }
 
                     memoryWarning++;
                 }
 
                 Thread.Sleep(1000);
-                
             }
-
         }// end Main
-
 
     }// end Class
 }// end Namespace
